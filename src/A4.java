7a3de238fee06e1f84277ba14b927918fb9edcbb
Emadmohammedsalah <111250396+Emadmohammedsalah@users.noreply.github.com>
2022-08-15 00:13:11 +0300

    Add files via upload

---
+++ src/A4.java
@@ -0,0 +1,6 @@
+public class A4 {
+    public static void main(String[] args) {
+        System.out.println((1000*0.05)*3+1000);
+
+    }
+}
---
+++ src/Q6.java
@@ -0,0 +1,7 @@
+public class Q6 {
+    public static void main(String[] args) {
+        System.out.println("___________");
+        System.out.println("|Emad Amer|");
+        System.out.println("___________");
+    }
+}
